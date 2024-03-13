dc4f4087deda8ad9e60a6320c1303ec8a26bbb44
Salym Senyonga <salymsash@gmail.com>
2024-03-13 23:11:12 +0300

    Add User show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/help',    to: 'static_pages#help'
   get '/signup',  to: 'users#new'
+  resources :users
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
