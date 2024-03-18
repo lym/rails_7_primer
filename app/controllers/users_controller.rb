cc46a44273ebc70a0bb443dad93be757fb3320ac
Salym Senyonga <salymsash@gmail.com>
2024-03-18 20:20:06 +0300

    Enable registration of new users
    When valid user attributes are entered into the signup form, create the
    new user in the database.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # TODO: Handle successful save
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -6,4 +15,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :name, :email, :password, :password_confirmation
+      )
+    end
 end
