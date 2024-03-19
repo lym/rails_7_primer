c580890a30f6e23abf05cab51ceb9bd5b91215fe
Salym Senyonga <salymsash@gmail.com>
2024-03-19 23:36:25 +0300

    Redirect to users show page after successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # TODO: Handle successful save
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,4 +20,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div.field_with_errors'
   end
+
+  test "successful signup" do
+    get signup_path
+    assert_difference 'User.count' do
+      post(
+        users_path,
+        params: {
+          user: {
+            name: "Big Baller",
+            email: "bigb@email.com",
+            password: 'foobarbaz',
+            password_confirmation: 'foobarbaz'
+        }}
+      )
+    end
+
+    assert_response :redirect
+    assert_select 'div.field_with_errors', 0
+  end
 end
