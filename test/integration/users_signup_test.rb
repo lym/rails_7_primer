0c3067f7cd0f51f2d1169ca8fd97689116adb088
Salym Senyonga <salymsash@gmail.com>
2024-03-19 22:40:15 +0300

    Add integration test for invalid signup attempts

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post(
+        users_path,
+        params: {
+          user: {
+            name: "",
+            email: "bad@email",
+            password: 'foo',
+            password_confirmation: 'bar'
+        }}
+      )
+    end
+
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div.field_with_errors'
+  end
+end
