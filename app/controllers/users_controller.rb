e721f61438667ac7c8bd7c4d82c3550aeccf8318
Salym Senyonga <salymsash@gmail.com>
2024-03-20 23:55:02 +0300

    Display flash message on successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Hey, you're in! Welcome, human."
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
