c28b13a6fde2aa0e3eafb6e332a3f359223884d2
Salym Senyonga <salymsash@gmail.com>
2024-03-23 01:02:28 +0300

    Add log in form UI

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/help',    to: 'static_pages#help'
   get '/signup',  to: 'users#new'
+  get '/login',   to: 'sessions#new'
+  post 'login',    to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
