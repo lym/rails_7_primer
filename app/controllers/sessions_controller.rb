6250179eb8f0da1d6e2ec0e9f9b30edaeef2af46
Salym Senyonga <salymsash@gmail.com>
2024-03-25 23:51:03 +0300

    Enable successful logins
    We're logging in the user with a temporary session cookie that expires
    automatically on browser close.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log user in and redirect to the user's show page
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
