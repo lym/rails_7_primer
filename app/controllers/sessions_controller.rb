d3d2c9ba1a1f4c2c51ca4c4695306782b2c384f0
Salym Senyonga <salymsash@gmail.com>
2024-03-25 23:21:52 +0300

    Handle invalid login form submissions
    Display a flash with an intuitive message when a login attempt is not
    successful.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log user in and redirect to the user's show page
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
   def new
   end
 end
