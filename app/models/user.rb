d17140892ef7e525ad14d5c238a5c671258246ed
Salym Senyonga <salymsash@gmail.com>
2024-04-11 23:54:12 +0300

    Handle successful user profile edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   # Remembers a user in the database. For use in persistent sessions
   def remember
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -20,4 +20,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select "div[class='field_with_errors']"
   end
+
+  test "user enters valid information in edit form" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "John Smith"
+    email = "jsmith@spies.com"
+    patch(
+      user_path(@user),
+      params: {
+        user: {
+          name: name, email: email, password: '', password_confirmation: ''
+        }
+      }
+    )
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
