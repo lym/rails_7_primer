a687e32e36f3df657fc5b8e286e728c125145941
Salym Senyonga <salymsash@gmail.com>
2024-05-10 21:17:36 +0300

    Enable post creation via the web UI.

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @micropost = microposts(:second_most_recent)
+  end
+
+  test "redirect non-logged-in users when they attempt to create microposts" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: {micropost: {content: "Test post"}}
+    end
+    assert_redirected_to login_url
+  end
+
+  test "redirect non-logged-in users when they attempt to delete microposts" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
 end
