8a66e67e1450c2a307f6ca951d8470c91c38a252
Salym Senyonga <salymsash@gmail.com>
2024-05-14 01:52:56 +0300

    Add image validation
    * Validate the format
    * Validate the file size
    * Add client-side size validation via JavaScript.
    * Hide non-suitable file types in the file picker

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "flowbite/flowbite.turbo.min"
+import "custom/image_upload"
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,13 @@ class Micropost < ApplicationRecord
 
   validates :user, presence: true
   validates :content, presence: true, length: {maximum: 140}
+
+  validates(
+    :image,
+    content_type: {
+      in: %w[image/jpeg image/gif image/png],
+      message: "must be a valid image format"
+    },
+    size: {less_than: 5.megabytes, message: "must be less than 5MB"}
+  )
 end
